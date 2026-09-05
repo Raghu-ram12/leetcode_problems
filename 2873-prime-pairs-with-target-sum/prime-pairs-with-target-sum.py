a657e76630545ef674647f62242a43dacf654a95
PENTELA RAGHU RAM <134118079+Raghu-ram12@users.noreply.github.com>
2026-09-05 20:31:30 +0530

    Time: 497 ms (95.44%) | Memory: 30.9 MB (96.09%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
@@ -1,22 +1,22 @@
+MAX=1000001
+primes=[True]*(MAX) 
+primes[0],primes[1]=False,False 
+
+i=2
+while i*i <= MAX:
+
+    if primes[i]:
+
+        for j in range(i*i,MAX,i):
+
+            primes[j]=False
+
+    i+=1 
+
 class Solution:
 
     def findPrimePairs(self, n: int) -> List[List[int]]:
-
-        primes=[True]*(n+1) 
-        primes[0],primes[1]=False,False 
-
-        i=2
-
-        while i*i <= n+1:
-
-            if primes[i]:
-
-                for j in range(i*i,n+1,i):
-
-                    primes[j]=False
-
-            i+=1 
-        
+ 
         
         result=[] 
 
